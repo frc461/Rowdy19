7ea806c42d1bfec83ff7eb4212a46ffc39d39bfc
Hank Krutulis <henrykrutulis@gmail.com>
2018-01-20 13:47:13 -0500

    Added makefile for command line build

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/DriveTrain.cpp \
+../src/Robot.cpp 
+
+OBJS += \
+./src/DriveTrain.o \
+./src/Robot.o 
+
+CPP_DEPS += \
+./src/DriveTrain.d \
+./src/Robot.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	arm-frc-linux-gnueabi-g++ -std=c++1y -I"/Users/hank/wpilib/cpp/current/include" -I"/Users/hank/eclipse-workspace/Rowdy19/src" -I"/Users/hank/wpilib/user/cpp/include" -O0 -Og -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
