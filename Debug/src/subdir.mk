0b345829aa079cd9f0ed802b4b90a75ebd97e428
Hank Krutulis <henrykrutulis@gmail.com>
2018-01-23 10:33:35 -0500

    Messed with make file

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -4,15 +4,12 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/DriveTrain.cpp \
 ../src/Robot.cpp 
 
 OBJS += \
-./src/DriveTrain.o \
 ./src/Robot.o 
 
 CPP_DEPS += \
-./src/DriveTrain.d \
 ./src/Robot.d 
 
 
