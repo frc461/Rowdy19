9bf24c946de597b72672a1b8006d4d85c493c083
Hank Krutulis <henrykrutulis@gmail.com>
2018-01-24 19:48:36 -0500

    Fixed command line build

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -4,20 +4,32 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/Robot.cpp 
+../src/Autonomous.cpp \
+../src/DriveTrain.cpp \
+../src/Robot.cpp \
+../src/Sensors.cpp \
+../src/ShuffleboardPoster.cpp 
 
 OBJS += \
-./src/Robot.o 
+./src/Autonomous.o \
+./src/DriveTrain.o \
+./src/Robot.o \
+./src/Sensors.o \
+./src/ShuffleboardPoster.o 
 
 CPP_DEPS += \
-./src/Robot.d 
+./src/Autonomous.d \
+./src/DriveTrain.d \
+./src/Robot.d \
+./src/Sensors.d \
+./src/ShuffleboardPoster.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	arm-frc-linux-gnueabi-g++ -std=c++1y -I"C:\Users\WBI/wpilib/cpp/current/include" -I"C:\Users\WBI\Documents\GitHub\Rowdy19\src" -I"C:\Users\WBI/wpilib/user/cpp/include" -O0 -Og -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	arm-frc-linux-gnueabi-g++ -std=c++1y -I"/Users/hank/wpilib/cpp/current/include" -I"/Users/hank/eclipse-workspace/Rowdy19/src" -I"/Users/hank/wpilib/user/cpp/include" -O0 -Og -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
